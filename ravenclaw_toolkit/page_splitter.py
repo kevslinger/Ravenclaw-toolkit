cf22d598e971a77cd1cff863111ea628b212b2ca
kevslinger <tug74186@temple.edu>
2020-12-05 21:42:35 -0500

    making both inputs required

--- ravenclaw_toolkit/page_splitter.py
+++ ravenclaw_toolkit/page_splitter.py
@@ -10,12 +10,12 @@ TITLES = ('Harry Potter and the Sorcerer\'s Stone',
          'Harry Potter and the Half-Blood Prince',
          'Harry Potter and the Deathly Hallows')
 
-OFFSETS = (12, 12, 14, 16, 16, 14, 17, )
-TOTAL_PAGES = (309, 341, 435, 734, 870, 652, 758)
-BOOKS = 'Books'
+OFFSETS = (12, 12, 14, 16, 16, 14, 16)
+TOTAL_PAGES = (309, 341, 435, 734, 870, 652, 759)
+STATIC = 'static'
 
-for idx in range(len(TITLES)):
-    inputpdf = PdfFileReader(open(os.path.join(BOOKS, TITLES[idx] + '.pdf'), 'rb'))
+for idx in range(6, len(TITLES)):
+    inputpdf = PdfFileReader(open(os.path.join(STATIC, TITLES[idx] + '.pdf'), 'rb'))
 
     for i in range(inputpdf.numPages):
         if i < OFFSETS[idx]:
@@ -24,7 +24,7 @@ for idx in range(len(TITLES)):
             break
         output = PdfFileWriter()
         output.addPage(inputpdf.getPage(i))
-        output_dir = os.path.join(BOOKS, f'HP{idx+1}')
+        output_dir = os.path.join(STATIC, f'HP{idx+1}')
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
         with open(os.path.join(output_dir, f'hp{(idx+1)}_{(i+1-OFFSETS[idx])}.pdf'), 'wb') as outputStream:
