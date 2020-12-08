6e549e3848bb02e5f487df87aa88b09735593648
kevslinger <tug74186@temple.edu>
2020-12-08 13:08:20 -0500

    Modularized validations for book/page/chapter

--- ravenclaw_toolkit/utils.py
+++ ravenclaw_toolkit/utils.py
@@ -20,7 +20,7 @@ def valid_book_number(book_number):
 
 # Obviously we cannot display a page before 1 or after the last page.
 def valid_page_number(book_number, page_number):
-    return 1<= page_number <= constants.PAGE_NUMBERS[book_number-1] # books are 1-indexed but the list is 0-indexed
+    return 1 <= page_number <= constants.TOTAL_PAGES[book_number-1] # books are 1-indexed but the list is 0-indexed
 
 
 # Make sure the page number the user enters is actually a valid page!
@@ -33,13 +33,66 @@ def validate_book_page_number(book_number, page_number):
     try:
         b = int(book_number)
         p = int(page_number)
+        assert b > 0
+        assert p > 0
         if valid_book_number(b) and valid_page_number(b, p):
             return b, p, 1
         else:
             return b, p, 0
-    except ValueError:
+    except (ValueError, AssertionError):
         return book_number, page_number, -1
 
+def validate_book_number(book_number):
+    try:
+        b = int(book_number)
+        assert b > 0
+        if valid_book_number(b):
+            return b, 1
+        else:
+            return b, 0
+    except (ValueError, AssertionError):
+        return book_number, -1
+
+def validate_page_number(page_number, booknum, chapternum=None):
+    try:
+        p = int(page_number)
+        assert p > 0
+        if chapternum:
+            # 0-indexed, first chapter
+            if chapternum == 1:
+                chapter_length = constants.CHAPTER_PAGE[booknum][chapternum] - 1
+            # Last chapter
+            elif chapternum == len(constants.CHAPTER_PAGE[booknum]):
+                chapter_length = constants.TOTAL_PAGES[booknum-1] - constants.CHAPTER_PAGE[booknum][chapternum-1]
+            # All other chapters
+            else:
+                chapter_length = constants.CHAPTER_PAGE[booknum][chapternum] - constants.CHAPTER_PAGE[booknum][chapternum-1]
+            if p <= chapter_length:
+                return p, 1
+            else:
+                return p, 0
+        else:
+            if valid_page_number(p, booknum):
+                return p, 1
+            else:
+                return p, 0
+    except (ValueError, AssertionError):
+        return page_number, -1
+
+def validate_chapter_number(chapter_number, booknum):
+    try:
+        c = int(chapter_number)
+        assert c > 0
+        if c <= len(constants.CHAPTER_PAGE[booknum]):
+            return c, 1
+        else:
+            return c, 0
+    except (ValueError, AssertionError):
+        return chapter_number, -1
+
+def get_page(booknum, chapternum, chapterpagenum):
+    return constants.CHAPTER_PAGE[booknum][chapternum-1] + chapterpagenum - 1
+    
     
 # util for binary search
 def calculate_mid(lower_bound, upper_bound):
@@ -52,6 +105,10 @@ def calculate_mid(lower_bound, upper_bound):
 # Chapter Page Num: integer, the page number we are at in the chapter
 def get_chapter_page(booknum, pagenum):
     chapter_list = constants.CHAPTER_PAGE[booknum]
+
+    # Last chapter
+    if pagenum >= chapter_list[-1]:
+        return len(chapter_list), pagenum - chapter_list[-1] + 1
     
     # We can use binary search to find the chapter, or use weird python list iteration...
     upper_bound = len(chapter_list) - 1
